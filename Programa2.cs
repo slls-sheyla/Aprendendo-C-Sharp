b8a49432e2b2cc5fcfbaf02d1900c2bb945317e2
Sheyla Lima <44805014+slls-sheyla@users.noreply.github.com>
2021-09-09 16:52:35 -0300

    Create Programa2.cs
    Tipos de variáveis

---
+++ Programa2.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Programa2
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 100;
+            
+            Console.WriteLine("a: " + a);
+            
+            double area = 0;
+            int salario, imposto, soma;
+            int contador = 10;
+            
+            Console.WriteLine("Valor 'contador': " + contador);
+            
+            string nome;
+            string nomeCompleto = "Visual Studipo";
+            
+            Console.ReadKey();
+
+            Console.WriteLine(nomeCompleto);
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
