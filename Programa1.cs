47475600548c336d86a627548a56472efeebf2c7
Sheyla Lima <slls.sheyla@gmail.com>
2021-09-09 19:40:33 -0300

    Primeiro programa usando a linguagem de programação C#

---
+++ Programa1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+namespace MeuPrimeiroProjeto
+{
+    class Programa1
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Olá mundo novo!");
+
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Meu nome é Sheyla");
+
+            Console.ReadKey();
+        }
+    }
+}
